3bee86158e57537af36f59653ee3295ffe3764f9
Abina Chowdhury <abina@etlimited.net>
2024-05-29 12:21:12 +0600

    unnecessary comment removed

--- index.js
+++ index.js
@@ -4,13 +4,12 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = 5000;
 
-// middleware
+
 app.use(cors());
 app.use(express.json());
 
 const uri = "mongodb+srv://abina:HIiGGA6OIoZs8G3Y@cluster0.948ms7y.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-// Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
         version: ServerApiVersion.v1,
@@ -20,16 +19,12 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    try {
-        // Connect the client to the server (optional starting in v4.7)
+    try { 
         await client.connect();
-
         const database = client.db("itemsDB");
         const itemCollection = database.collection('items');
 
-        // Handle GET request to fetch items
         app.get('/items', async (req, res) => {
-            console.log('/items api get korteci');
             try {
                 const cursor = await itemCollection.find().toArray();
                 res.json(cursor);
@@ -39,40 +34,31 @@ async function run() {
             }
         });
 
-        // Handle POST request to insert an item
+       
         app.post('/items', async (req, res) => {
-            console.log('/items api post korteci');
-
             const item = req.body;
             try {
                 const result = await itemCollection.insertOne(item);
-                console.log(result);
                 res.send(result);
             } catch (error) {
                 console.log(error);
                 res.status(500).send({ message: "Failed to insert item", error });
             }
-
         });
 
         app.get('/items/:id', async (req, res) => {
-            console.log('/items/:id api get korteci');
-
             const singleId = req.params;
             const singleData = await itemCollection.findOne({ _id: new ObjectId(singleId) });
             res.send(singleData);
         })
 
         app.delete('/items/:id', async (req, res) => {
-            console.log('/items/:id api delete korteci');
-
             const singleId = req.params;
             const singleDataDelete = await itemCollection.deleteOne({ _id: new ObjectId(singleId) });
             res.send(singleDataDelete);
         })
 
         app.put('/items/:id', async (req, res) => {
-            console.log('/items/:id api update korteci');
             const singleId = req.params.id;
             const updatedItem = req.body;
             const singleDataUpdate = await itemCollection.updateOne(
@@ -91,14 +77,10 @@ async function run() {
             });
         })
 
-
-
-        // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-        // Ensures that the client will close when you finish/error
-        // await client.close();
+        
     }
 }
 run();
